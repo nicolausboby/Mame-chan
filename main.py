edd620b3ee81d84c9c4d09ea2b2bc845f7fddb04
Nicolaus Boby <nicolausboby@gmail.com>
2021-01-15 17:55:00 +0700

    init bot

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+import requests
+import json
+# import MySQLdb
+from decouple  config
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	if message.content.startswith('$hello'):
+		await message.channel.send('Hello!')
+
+token = config('TOKEN')
+
+client.run(token)
